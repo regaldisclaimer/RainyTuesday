843947221034dcdc7df77d9547b4794ad3b5f3e7
Dan Meyer <djmeyer96@gmail.com>
2015-01-13 01:15:07 -0500

    Sets up index.js for firebase js functions and sets up makeAccount function

---
+++ index.js
@@ -0,0 +1,17 @@
+// Dean's List Firebase JS scripts
+// v 0.1
+// @dyz3
+
+<script src="https://cdn.firebase.com/js/client/2.1.1/firebase.js">
+
+var myFirebaseRef = new Firebase("https://deans.firebaseio.com/");
+var Firebase = require("firebase");
+
+// Function for making an account
+function makeAccount(email, name) {
+	push email;
+	if (name != null)
+		push name;	
+}
+
+</script>
